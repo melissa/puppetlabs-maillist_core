ebf8c97292142499733a22694f1249d087687fc9
Melissa Stone <melissa@puppet.com>
2018-04-19 16:26:01 -0700

    Fix unit tests

--- spec/unit/type/maillist_spec.rb
+++ spec/unit/type/maillist_spec.rb
@@ -4,38 +4,36 @@ require 'spec_helper'
 maillist = Puppet::Type.type(:maillist)
 
 describe maillist do
-  before do
-    @provider_class = Puppet::Type.type(:maillist).provider(:mailman)
+  before(:each) do
+    let(:provider_class) { Puppet::Type.type(:maillist).provider(:mailman) }
 
-    @provider = stub 'provider', :class => @provider_class, :clear => nil
-    @provider.stubs(:respond_to).with(:aliases).returns(true)
+    let(:provider) { stub 'provider', class: provider_class, clear: nil }
+    provider.stubs(:respond_to).with(:aliases).returns(true)
 
-    @provider_class.stubs(:new).returns(@provider)
+    provider_class.stubs(:new).returns(provider)
 
-    Puppet::Type.type(:maillist).stubs(:defaultprovider).returns(@provider_class)
+    Puppet::Type.type(:maillist).stubs(:defaultprovider).returns(provider_class)
 
-    @maillist = Puppet::Type.type(:maillist).new( :name => 'test' )
+    let(:maillist) { Puppet::Type.type(:maillist).new(name: 'test') }
+    let(:catalog) { Puppet::Resource::Catalog.new }
 
-    @catalog = Puppet::Resource::Catalog.new
-    @maillist.catalog = @catalog
+    maillist.catalog = catalog
   end
 
-  it "should generate aliases unless they already exist" do
+  it 'generates aliases unless they already exist' do
     # Mail List aliases are careful not to stomp on managed Mail Alias aliases
 
     # test1 is an unmanaged alias from /etc/aliases
-    Puppet::Type.type(:mailalias).provider(:aliases).stubs(:target_object).returns( StringIO.new("test1: root\n") )
+    Puppet::Type.type(:mailalias).provider(:aliases).stubs(:target_object).returns(StringIO.new("test1: root\n"))
 
     # test2 is a managed alias from the manifest
-    dupe = Puppet::Type.type(:mailalias).new( :name => 'test2' )
-    @catalog.add_resource dupe
+    dupe = Puppet::Type.type(:mailalias).new(name: 'test2')
+    catalog.add_resource dupe
 
-    @provider.stubs(:aliases).returns({"test1" => 'this will get included', "test2" => 'this will dropped', "test3" => 'this will get included'})
+    provider.stubs(:aliases).returns('test1' => 'this will get included', 'test2' => 'this will dropped', 'test3' => 'this will get included')
 
-    generated = @maillist.generate
-    expect(generated.map{ |x| x.name  }.sort).to eq(['test1', 'test3'])
-    expect(generated.map{ |x| x.class }).to      eq([Puppet::Type::Mailalias, Puppet::Type::Mailalias])
-
+    generated = maillist.generate
+    expect(generated.map { |x| x.name  }.sort).to eq(%w[test1 test3])
+    expect(generated.map { |x| x.class }).to      eq([Puppet::Type::Mailalias, Puppet::Type::Mailalias])
   end
-
 end
